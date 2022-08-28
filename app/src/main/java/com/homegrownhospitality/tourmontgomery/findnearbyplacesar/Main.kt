baf54e500541e60b5b9c80aa97f94e4940ad247e
Acidion <acidburn947@gmail.com>
2022-08-27 23:44:23 -0500

    Initial Commit

---
+++ app/src/main/java/com/homegrownhospitality/tourmontgomery/findnearbyplacesar/Main.kt
@@ -0,0 +1,74 @@
+package com.homegrownhospitality.tourmontgomery.findnearbyplacesar
+
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class Main : AppCompatActivity() {
+
+    private lateinit var spinnerFragment : Spinner
+    private var locSearchType : String = "restaurant"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!isSupportedDevice()) {
+            return
+        }
+
+        setContentView(R.layout.activity_main)
+
+        spinnerFragment = findViewById(R.id.locType)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.locTypes,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerFragment.adapter = adapter
+        }
+
+        val button: Button = findViewById(R.id.btnOpenAR)
+        button.setOnClickListener {
+            val intent = Intent(this, ArView::class.java)
+            intent.putExtra("placeType", locSearchType)
+            startActivity(intent)
+        }
+    }
+
+    private fun setUpSpinner () {
+        spinnerFragment.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                                        view: View, position: Int, id: Long) {
+                locSearchType = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>){
+
+            }
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    private fun isSupportedDevice(): Boolean {
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val openGlVersionString = activityManager.deviceConfigurationInfo.glEsVersion
+        if (openGlVersionString.toDouble() < 3.0) {
+            Toast.makeText(this, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                .show()
+            finish()
+            return false
+        }
+        return true
+    }
+}
